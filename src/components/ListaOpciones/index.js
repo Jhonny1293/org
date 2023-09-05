e022b0e69771f3fcfe3c1fdab8afd5799d8e425a
Jhonatan <jhona_re_mo@hotmail.com>
2023-09-05 12:02:06 -0500

    Terminando de completar faltantes del curso

---
+++ src/components/ListaOpciones/index.js
@@ -0,0 +1,23 @@
+import "./ListaOpciones.css"
+
+const ListaOpciones = (props) =>{
+
+    //Método map -> arreglo.map( (equipo, index) ) =>{
+        // return <option></opcion>
+    // })
+
+    const manejarCambio = (e) =>{
+        console.log("cambio", e.target.value)
+        props.actualizarEquipo(e.target.value)
+    }
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select value={props.valor} onChange={manejarCambio}>
+            <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
+            {props.equipos.map((equipos, index)=> <option key={index} value={equipos}>{equipos}</option>)}
+        </select>
+    </div>
+}
+
+export default ListaOpciones
